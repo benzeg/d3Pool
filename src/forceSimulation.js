f785c61e7734d660eecb83f973f6c0d2e1c97b10
Benze Gong <benzebgong@gmail.com>
2017-05-28 23:16:07 -0700

    added constructor params and apply force function

--- src/forceSimulation.js
+++ src/forceSimulation.js
@@ -4,6 +4,10 @@ class forceSimulation {
 	constructor() {
 		this.nodes = null;
 		this.events = {};
+		this.friction = new Victor(1, 1);
+		this.timeVec = new Victor(0.01, 0.01);
+		this.forceVec = [];
+		this.moved = 0;
 	}
 
 	on(event, cb) {
@@ -13,6 +17,34 @@ class forceSimulation {
 	emit(event) {
 		if (this.events[event]) {
 			this.events[event]();
+		}
+	}
+
+	addNodes(nodes) {
+		this.nodes = nodes;
+	}
+
+	applyForce(forceVec) {
+		this.forceVec.push(forceVec);
+		this.move = setInterval(() => {
+			this.nodes.forEach(node, index) {
+				if (this.forceVec[index]) {
+					var transVec = this.forceVec[index].clone().multiply(this.timeVec);
+					node.cx += transVec.x;
+					node.cy += transVec.y;
+					this.moved[index] = 1;
+				} 
+			}
+
+			forceVec.subtract(this.friction);
+
+		})
+	}
+
+	checkCollision() {
+		var i = 0;
+		while (i < this.nodes.length) {
+
 		}
 	}
 }
