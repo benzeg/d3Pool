1696afb94cf3f9f7a9d49f3c14a19c31039759e1
Benze Gong <benzebgong@gmail.com>
2017-05-29 17:15:21 -0700

    updated to forEach loops to improve readability

--- src/forceSimulation.js
+++ src/forceSimulation.js
@@ -8,6 +8,7 @@ class forceSimulation {
 		this.forceVec = [];
 		//**refactor to bitwise operation
 		this.moved = [];
+		this.run = true;
 	}
 
 	on(event, cb) {
@@ -18,6 +19,14 @@ class forceSimulation {
 		if (this.events[event]) {
 			this.events[event]();
 		}
+	}
+
+	resume() {
+		this.run = true;
+	}
+
+	pause() {
+		this.run = false;
 	}
 
 	addNodes(nodes) {
@@ -33,31 +42,34 @@ class forceSimulation {
 	applyForce(forceVec) {
 		this.forceVec.push(forceVec);
 		this.move = setInterval(() => {
-			//check collision
-			updateAllNodes();
-			if (!checkForceVec()) {
-				clearInterval(this.move);
-			} else {
-				//apply force vector to node position
-				this.nodes.forEach(node, index) {
-					if (this.forceVec[index]) {
-						//calculate translation vector here
-						var transVec = this.forceVec[index].clone().multiply(this.timeVec);
-						node.cx += transVec.x;
-						node.cy += transVec.y;
-						//moved marks if a node has an active movement at the present time
-						this.moved[index] = 1;
-						//apply friction to force vector
-						var friction = this.forceVec[index].clone().normalize();
-						this.forceVec[index].subtract(friction);
-						//remove force vector if it becomes stationary
-						if (this.forceVec[index].x === 0 && this.forceVec[index].y === 0) {
-							this.forceVec[index] = undefined;
-						}
-					} 
+			if(this.run) {
+				//check collision
+				updateAllNodes();
+				if (!checkForceVec()) {
+					clearInterval(this.move);
+				} else {
+					//apply force vector to node position
+					this.nodes.forEach((node, index) =>{
+						if (this.forceVec[index]) {
+							//calculate translation vector here
+							var transVec = this.forceVec[index].clone().multiply(this.timeVec);
+							node.cx += transVec.x;
+							node.cy += transVec.y;
+							//moved marks if a node has an active movement at the present time
+							this.moved[index] = 1;
+							//apply friction to force vector
+							var friction = this.forceVec[index].clone().normalize();
+							this.forceVec[index].subtract(friction);
+							//remove force vector if it becomes stationary
+							if (this.forceVec[index].x === 0 && this.forceVec[index].y === 0) {
+								this.forceVec[index] = undefined;
+							}
+						} 
+					}
+					//emit event to update dom
+					this.emit('tick');
+					this.pause();
 				}
-				//emit event to update dom
-				emit('ticked');
 			}
 		}, 40);
 
@@ -65,12 +77,12 @@ class forceSimulation {
 	}
 
 	updateAllNodes() {
-		this.nodes.forEach(node, index) {
+		this.nodes.forEach((node, index) => {
 			if (this.moved[index] === 1) {
 				checkCollision(index);
 				this.moved[index] = 0;
 			}
-		}
+		});
 		checkWallCollision();
 	}
 
@@ -79,7 +91,7 @@ class forceSimulation {
 		var r1 = this.nodes[currIndex].r;
 		var cx1 = this.nodes[currIndex].cx;
 		var cy1 = this.nodes[currIndex].cy;
-		this.nodes.forEach(node, index) {
+		this.nodes.forEach((node, index) => {
 			if (index !== currIndex) {
 				//two circles collide if Square(r1 + r2) < Square(x1 - x2) + Sqaure(y1 - y2)
 				var r2 = node.r;
@@ -95,12 +107,12 @@ class forceSimulation {
 					this.forceVec[currIndex] = undefined;
 				}
 			}
-		}
+		});
 	}
 
 	checkWallCollision() {
 		//currently just stops if hits a wall
-		this.nodes.forEach(node, index) {
+		this.nodes.forEach((node, index) => {
 			if (node.cx < 16.25) {
 				node.cx = 16.25;
 				this.forceVec[index] = undefined;
@@ -116,15 +128,15 @@ class forceSimulation {
 				node.cy = 543.75;
 				this.forceVec[index] = undefined;
 			}
-		}
+		});
 	}
 
 	checkForceVec() {
-		this.forceVec.forEach(force) {
+		this.forceVec.forEach((force) => {
 			if (force !== undefined) {
 				return true;
 			}
-		}
+		});
 		return false;
 	}
 }
