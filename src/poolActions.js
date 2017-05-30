e27f7346e57b1873907eb41e0ea390661c68ad9e
Benze Gong <benzebgong@gmail.com>
2017-05-29 18:50:38 -0700

    adjusted cueball max velocity and call on force simulation

--- src/poolActions.js
+++ src/poolActions.js
@@ -1,7 +1,7 @@
 var Victor = require('victor');
-var SetForce = require('./ballMove.js').setForce;
+var SetForce = require('./simulation.js').setForce;
 
-var setMouseEvent = function(board, powerGrid, cueBall, gameBall) {
+var setMouseEvent = function(board, powerGrid, ball) {
 	var table = board.getTable();
 
 	table.on('mousedown', () => {
@@ -20,17 +20,15 @@ var setMouseEvent = function(board, powerGrid, cueBall, gameBall) {
 		}, 10);
 	}).on('mouseup', () => {
 	//fetch cueball and gameball nodes to feed into transition
-		var cueballNodes = cueBall.getNodes();
-		var gameballNodes = gameBall.getNodes();
+		var ballNodes = ball.getNodes();
 		//fetch cueball and gameball svg elements to feed into transition
-		var cueSVG = cueBall.getModel();
-		var gameSVG = gameBall.getModel();
+		var ballSVG = ball.getModel();
 
 		//max force: 50N, max velocity: 1500px/sec
-		var Vmax = (this.powerBarHeight / 240) * 4500;
+		var Vmax = (this.powerBarHeight / 240) * 300;
 		this.velVec = Victor.fromArray([Vmax, Vmax]);
 			//get cueball position
-		this.cueBallVec = Victor.fromArray(cueBall.getPosition());
+		this.cueBallVec = Victor.fromArray(ball.getCueBallPosition());
 		//generate force vector
 		this.forceVec = this.cueBallVec.clone();
 		this.forceVec.subtract(this.mouseVec);
@@ -38,75 +36,84 @@ var setMouseEvent = function(board, powerGrid, cueBall, gameBall) {
 		this.forceVec.normalize();
 		//add travel distance
 		this.forceVec.multiply(this.velVec);
-		var move = () => {
 
-			//calculate translation position
-			var translateVec = this.cueBallVec.clone().add(this.forceVec);
-			//check collision with other balls
+		/////////////////////////////////////////////////////////
+		//NEW CODE
 
-			//remaining force;
-			var reverseVec;
-			//check bounds and invert force vector
-			if (translateVec.x < 16.25) {
-				var diffX = -16.25 + translateVec.x;
-				var ratio = diffX / this.forceVec.x;
-				var diffY = ratio * this.forceVec.y;
-				reverseVec = Victor.fromArray([-diffX, -diffY]);
-				this.forceVec.add(reverseVec);
-				translateVec = this.cueBallVec.clone().add(this.forceVec);
-			} else if (translateVec.x > 1120 - 16.25) {
-				var diffX = 16.26 + (translateVec.x - 1120);
-				var ratio = diffX / this.forceVec.x;
-				var diffY = ratio * this.forceVec.y;
-				reverseVec = Victor.fromArray([-diffX, -diffY]);
-				this.forceVec.add(reverseVec);
-				translateVec = this.cueBallVec.clone().add(this.forceVec);
-			}
+		SetForce(ballSVG, ballNodes, this.forceVec);
 
-			if (translateVec.y < 16.25) {
-				var diffY = -16.25 + translateVec.y;
-				var ratio = diffY / this.forceVec.y;
-				var diffX = ratio * this.forceVec.x;
-				reverseVec = Victor.fromArray([-diffX, -diffY]);
-				this.forceVec.add(reverseVec);
-				translateVec = this.cueBallVec.clone().add(this.forceVec);
-			} else if (translateVec.y > 560 - 16.25) {
-				var diffY = 16.25 + (translateVec.y - 560);
-				var ratio = diffY / this.forceVec.y;
-				var diffX = ratio * this.forceVec.x;
-				reverseVec = Victor.fromArray([-diffX, -diffY]);
-				this.forceVec.add(reverseVec);
-				translateVec = this.cueBallVec.clone().add(this.forceVec);
-			}
 
-			if (translateVec.x === 16.25 || translateVec.x === 1120 - 16.25) {
+		////////////////////////////////////////////////////////////////////////////
+		//OLD CODE
+		// var move = () => {
+
+		// 	//calculate translation position
+		// 	var translateVec = this.cueBallVec.clone().add(this.forceVec);
+		// 	//check collision with other balls
+
+		// 	//remaining force;
+		// 	var reverseVec;
+		// 	//check bounds and invert force vector
+		// 	if (translateVec.x < 16.25) {
+		// 		var diffX = -16.25 + translateVec.x;
+		// 		var ratio = diffX / this.forceVec.x;
+		// 		var diffY = ratio * this.forceVec.y;
+		// 		reverseVec = Victor.fromArray([-diffX, -diffY]);
+		// 		this.forceVec.add(reverseVec);
+		// 		translateVec = this.cueBallVec.clone().add(this.forceVec);
+		// 	} else if (translateVec.x > 1120 - 16.25) {
+		// 		var diffX = 16.26 + (translateVec.x - 1120);
+		// 		var ratio = diffX / this.forceVec.x;
+		// 		var diffY = ratio * this.forceVec.y;
+		// 		reverseVec = Victor.fromArray([-diffX, -diffY]);
+		// 		this.forceVec.add(reverseVec);
+		// 		translateVec = this.cueBallVec.clone().add(this.forceVec);
+		// 	}
+
+		// 	if (translateVec.y < 16.25) {
+		// 		var diffY = -16.25 + translateVec.y;
+		// 		var ratio = diffY / this.forceVec.y;
+		// 		var diffX = ratio * this.forceVec.x;
+		// 		reverseVec = Victor.fromArray([-diffX, -diffY]);
+		// 		this.forceVec.add(reverseVec);
+		// 		translateVec = this.cueBallVec.clone().add(this.forceVec);
+		// 	} else if (translateVec.y > 560 - 16.25) {
+		// 		var diffY = 16.25 + (translateVec.y - 560);
+		// 		var ratio = diffY / this.forceVec.y;
+		// 		var diffX = ratio * this.forceVec.x;
+		// 		reverseVec = Victor.fromArray([-diffX, -diffY]);
+		// 		this.forceVec.add(reverseVec);
+		// 		translateVec = this.cueBallVec.clone().add(this.forceVec);
+		// 	}
+
+		// 	if (translateVec.x === 16.25 || translateVec.x === 1120 - 16.25) {
 				
-			}
+		// 	}
 
-			if (translateVec.y === 16.25 || translateVec.y === 560 - 16.25) {
+		// 	if (translateVec.y === 16.25 || translateVec.y === 560 - 16.25) {
 				
-			}
+		// 	}
 
-			cueballNodes[0].cx = translateVec.x;
-			cueballNodes[0].cy = translateVec.y;
+		// 	cueballNodes[0].cx = translateVec.x;
+		// 	cueballNodes[0].cy = translateVec.y;
 			
-			//SetForce(gameSVG, gameballNodes);
+		// 	//SetForce(gameSVG, gameballNodes);
 
-			//translate
-			cueBall.translate(cueballNodes, () => {
-				//update cueBall position
-				//this.cueBallVec = Victor.fromArray(cueBall.getPosition());
-				// if (Vmax > 0) {
-				// 	move();
-				// } else {
-				// 	return;
-				// }
-				console.log('done')
-			});
+		// 	//translate
+		// 	cueBall.translate(cueballNodes, () => {
+		// 		//update cueBall position
+		// 		//this.cueBallVec = Victor.fromArray(cueBall.getPosition());
+		// 		// if (Vmax > 0) {
+		// 		// 	move();
+		// 		// } else {
+		// 		// 	return;
+		// 		// }
+		// 		console.log('done')
+		// 	});
 
-		}
+		//}
 
-		move();
+		//move();
 
 		//clear increment interval
 		this.powerBarHeight = 0;
