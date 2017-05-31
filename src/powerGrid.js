957f3ff03970126a94fcfb3940f6f57813fd2f12
Benze Gong <benzebgong@gmail.com>
2017-05-31 14:39:45 -0700

    add comments and var name changes

--- src/powerGrid.js
+++ src/powerGrid.js
@@ -1,32 +1,79 @@
+/*Dependencies*/
+var Victor = require('victor'); //vector library
+
+/*CODE*/
 class PowerGrid {
-	constructor(board) {
-		this.board = board;
-		this.container = null;
+	constructor(SVGContainer) {
+		//STYLING
+		this.powerGridOptions = {
+			height: 240,
+			width: 20,
+			x: 1120,
+			y: 80,
+			fill: '#ffffff'
+		}
+
+		this.powerLevelOptions = {
+			height: 0,
+			width: 20,
+			x: 1120,
+			y: 80,
+			fill: '#8c55555'
+		}
+
+		//SVGs
+		this.Container = SVGcontainer;
 		this.powerGrid = null;
+		this.powerLevel = null;
+
+		//CACHE
+		this.maxVelocity = 300; //preset max velocity of 300px/sec
+		this.currentPower = null;
 	}
 
+	//generates new power grid svg and adds to pool table svg
+	//Grid is set up to include an outer rectangular box with set height and white background
+	//and an inner grid with initial height of 0, inner grid's height increases dynamically
+	//to reflect a cue hit's power level
 	setUp() {
-		this.container = this.board.append('svg:rect')
-			.attr('height', 240)
-			.attr('width', 20)
-			.attr('x', 1080)
-			.attr('y', 40)
-			.attr('fill', '#ffffff')
+		//populate SVG models
+		this.powerGrid = this.Container.append('svg:rect')
+			.attr('height', this.powerGridOptions.height)
+			.attr('width', this.powerGridOptions.width)
+			.attr('x', this.powerGridOptions.x)
+			.attr('y', this.powerGridOptions.y)
+			.attr('fill', this.powerGridOptions.fill);
 
-		this.powerGrid = this.board.append('svg:rect')
-			.attr('height', 0)
-			.attr('width', 20)
-			.attr('x', 1080)
-			.attr('y', 40)
-			.attr('fill', '#8c5555')
+		this.powerLevel = this.Container.append('svg:rect')
+			.attr('height', this.powerLevelOptions.height)
+			.attr('width', this.powerLevelOptions.width)
+			.attr('x', this.powerLevelOptions.x)
+			.attr('y', this.powerLevelOptions.y)
+			.attr('fill', this.powerLevelOptions.fill)
+
+		//populate cache
+		this.currentPower = 0;
+		this.maxPower = this.powerGridOptions.height;
+
+		return;
 	}
 
-	getHeight() {
-		return this.powerGrid.attr('height');
+	//converts current power level to velocity vector by comparing current power level to max power level
+	getPower() {
+		let v = (this.currentPower / this.maxPower) * this.maxVelocity;
+		let velocityVec = new Victor.fromArray([v, v]);
+		return velocityVec;
 	}
 
-	setHeight(height) {
-		this.powerGrid.attr('height', height);
+	//increases power level
+	incrementPower() {
+		if (this.currentPower === this.powerGrid.attr('height')) {
+			return 'Error: At maximum power';
+		}
+		//only increment while less than max power
+		this.currentPower = this.powerLevel.attr('height'); //update cache
+		this.powerLevel.attr('height', this.currentPower++);
+		return this.currentPower;
 	}
 }
 
