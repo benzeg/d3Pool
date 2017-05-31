6efab192d8ecce52d4b456f61c64c20e3aeafc29
Benze Gong <benzebgong@gmail.com>
2017-05-31 15:56:38 -0700

    removed pocket initiation from table class

--- src/poolTable.js
+++ src/poolTable.js
@@ -1,32 +1,60 @@
+/*Dependencies*/
 var d3 = require('d3');
-
-class Table {
+/////////////////////////////////////////////////////////////////////////////
+class Init {
 	constructor(container) {
+		//STYLING
+		this.innerStyle = {
+			x: 40,
+			y: 40,
+			width: 1120,
+			height: 560,
+			color: '#669133',
+		};
+
+		this.outerStyle = {
+			width: 1200,
+			height: 640,
+			color: '#8a3232'
+		};
+
+		//html container
 		this.container = container;
-		this.xLeft = 0;
-		this.xRight = 1120;
-		this.yTop = 0;
-		this.yBottom = 560;
-		this.color = '#648fcb';
+
+		//MODEL
 		this.model = null;
+		this.playArea = null;
 	}
 
-	setTable() {
+	/*
+	Generates an outer container representing the entire pool table with
+	smaller container inside representing the play area.
+	*/
+	setUp() {
 		this.model = d3.select(container).append('svg:svg')
-			.attr('height', this.yBottom)
-			.attr('width', this.xRight)
-			.style('background-color', this.color);
+			.attr('height', this.outerStyle.height)
+			.attr('width', this.outerStyle.width)
+			.style('background-color', this.outerStyle.color);
+
+		this.playArea = this.model.append('svg:rect')
+			.attr('height', this.innerStyle.height)
+			.attr('width', this.innerStyle.width)
+			.attr('x', this.innerStyle.x)
+			.attr('y', this.innerStyle.y)
+			.style('fill', this.innerStyle.color);
+
+		return this.model;
 	}
 
 	getMouseCoordinates() {
 		return d3.mouse(this.model._groups[0][0]);
 	}
 
-	getTable() {
+	getModel() {
 		return this.model;
 	}
 }
 
 module.exports = {
-	Table: Table
+	Init: Init
 }
