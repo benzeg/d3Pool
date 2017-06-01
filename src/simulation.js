3e67fc91bf0aaf0e5bcb2160cf0cf74a1c6fb7b2
Benze Gong <benzebgong@gmail.com>
2017-05-31 20:12:34 -0700

    refactored simulation initiation

--- src/simulation.js
+++ src/simulation.js
@@ -1,21 +1,20 @@
-var Victor = require('victor');
-var ForceSimulation = require('./forceSimulation.js').forceSimulation;
+/*Dependencies*/
+const Victor = require('victor');
+const ForceSimulation = require('./forceSimulation.js').forceSimulation;
+////////////////////////////////////////////////////////////////
 
-var setSimulation = (ballSVG, ballOptions, cueForce) => {
-
-	function redraw() {
-		ballSVG.attr("cx", function(d) {return d.cx});
-		ballSVG.attr("cy", function(d) {return d.cy});
+const Init = (nodes, cueForce, tickEvent) => {
+	const cb = function() {
 		simulation.resume();
 	}
 
-	var simulation = new ForceSimulation();
-	simulation.addNodes(ballOptions)
-						.on('tick', redraw);
+	const simulation = new ForceSimulation();
+	simulation.addNodes(nodes)
+						.on('tick', () => { tickEvent(cb); });
 
 	simulation.applyForce(cueForce);
 }
 
 module.exports = {
-	setForce: setSimulation
+	init: Init
 }
