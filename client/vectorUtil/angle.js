fafe8e6c70aeef0f7bb82b39e9ba0f5415c8d879
Benze Gong <benzebgong@gmail.com>
2018-12-16 17:28:36 -0800

    test passes for getDirection

--- client/vectorUtil/angle.js
+++ client/vectorUtil/angle.js
@@ -37,13 +37,58 @@ function isNegative(n){
     return n < 0
 };
 
-function testFn() {
+function getTheta_test() {
     assert(
         Math.abs( getTheta( -Math.PI*2 - Math.PI*0.5 - 1 ) - ( Math.PI*1.5 - 1 ) < Math.PI/360 ),
         "Should return the theta value given a radian value which may be negative and/or contains multiple full angular rotations, while keeping its inaccuracy no greater than 0.5 of an anglular degree." );
 }
 
-testFn();
+getTheta_test();
+
+function getDirection(r){
+    const quadrant = [
+        Math.PI*0.5,
+        Math.PI,
+        Math.PI*1.5,
+        Math.PI*2
+    ];
+    
+    const theta = normalize( r );
+
+    if( !(theta > quadrant[0]) ) {
+       if(quadrant[0] - theta === 0){
+           return [0, 1];
+       }
+       return [1, 1]; 
+    } else if( !(theta > quadrant[1]) ) {
+        if(quadrant[1] - theta === 0){
+            return [-1, 0];
+        }
+        return [-1, 1];
+    } else if( !(theta > quadrant[2]) ) {
+        if(quadrant[2] - theta === 0){
+            return [0, -1];
+        }
+        return [-1, -1]; 
+    } else {
+        if(theta === 0 || quadrant[3] - theta === 0){
+            return [0, 0];
+        }
+        return [1, -1];
+    }
+}
+
+function getDirection_test(){
+    assert.deepStrictEqual(
+        getDirection( -Math.PI*2 - Math.PI*0.5 - 1 ),
+        [-1, -1],
+        "Should return the correct quadrant given a radian value which may be negative and/or contains multiple full angular rotations."
+    )
+}
+
+getDirection_test();
+
 module.exports = {
-    getTheta: getTheta
+    getTheta: getTheta,
+    getDirection: getDirection
 };
