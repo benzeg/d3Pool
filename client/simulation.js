4eda6e74f897f9c3038dd688e2ea0aad7129cc21
Benze Gong <benzebgong@gmail.com>
2018-12-17 21:17:22 -0800

    format chore

--- client/simulation.js
+++ client/simulation.js
@@ -5,11 +5,11 @@ import ForceSimulation from './forceSimulation';
 //** todo refactor to class
 
 const Simulate = (activeNodes, cueForce, tickEvent, catchEvent, cueResume) => {
+
+	const simulation = new ForceSimulation();
 	const cb = () => {
 		simulation.resume();
 	}
-
-	const simulation = new ForceSimulation();
 	simulation.addNodes(activeNodes)
 						.on('tick', () => { tickEvent(cb); })
 						.on('catch', (id) => { catchEvent(id); });
